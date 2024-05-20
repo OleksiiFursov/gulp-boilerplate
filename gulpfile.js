522e229639212f3fc1a4350a38897c68306d76e4
OleksiiFursov <nodePro777@gmail.com>
2024-05-21 00:18:35 +0200

    add folders

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,7 @@ import './gulp/build.js';
 const tasks = ['html', 'sass', 'images', 'fonts', 'files', 'js', 'pwa'];
 
 const devTasks = gulp.parallel(...tasks.map(v=> v+':dev'));
-const buildTasks = gulp.parallel(...tasks.map(v=> v+':build'));
+const buildTasks = gulp.parallel(...tasks.map(v=> v+':build'), 'generate-webmanifest', 'generate-favicon');
 
 gulp.task('default', gulp.series('clean:dev', devTasks, gulp.parallel('server:dev', 'watch:dev')));
 gulp.task('build', gulp.series('clean:build', buildTasks));
