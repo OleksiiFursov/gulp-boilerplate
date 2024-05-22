7e51120749a26131172afe0f0b8bcab3a932fade
OleksiiFursov <nodepro777@gmail.com>
2024-05-22 10:58:05 +0200

    fix build

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,7 @@ import './gulp/build.js';
 const tasks = ['html', 'sass', 'images', 'fonts', 'files', 'js', 'pwa'];
 
 const devTasks = gulp.parallel(...tasks.map(v=> v+':dev'));
-const buildTasks = gulp.parallel(...tasks.map(v=> v+':build'), 'generate-webmanifest', 'generate-favicon');
+//const buildTasks = gulp.parallel(...tasks.map(v=> v+':build'), 'generate-webmanifest', 'generate-favicon');
 
 gulp.task('default', gulp.series('clean:dev', devTasks, gulp.parallel( 'serve:dev')));
-gulp.task('build', gulp.series('clean:build', buildTasks));
+//gulp.task('build', gulp.series('clean:build', buildTasks));
