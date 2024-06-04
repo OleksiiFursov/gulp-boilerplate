ae86173d135bec72f9e7ceb6d6e6ac1af2074a07
OleksiiFursov <nodepro777@gmail.com>
2024-06-04 17:33:51 +0200

    dev

--- gulp/task.js
+++ gulp/task.js
@@ -2,6 +2,8 @@ import fs from 'fs'
 import gulp from 'gulp'
 import changed from 'gulp-changed'
 import clean from 'gulp-clean'
+import realFavicon from 'gulp-real-favicon'
+import config from '../config.js'
 import { getBuildDir, getSrcDir } from './tools.js'
 
 export const clearBuild = done => {
@@ -27,3 +29,72 @@ export const movePWA = () =>
       .pipe(changed(getBuildDir()))
       .pipe(gulp.dest(getBuildDir()))
 
+
+export const generateFavicon = (done, check=true) =>  {
+	const logoPath = getSrcDir('img/cf-favicon.png');
+
+	if (fs.existsSync(logoPath) && fs.existsSync(getSrcDir('pwa'))) {
+		realFavicon.generateFavicon({
+			masterPicture: logoPath,
+			dest: getBuildDir('pwa'),
+			design: {
+				ios: {
+					pictureAspect: 'backgroundAndMargin',
+					backgroundColor: config.THEME_BACKGROUND,
+					margin: '14%',
+					assets: {
+						ios6AndPriorIcons: false,
+						ios7AndLaterIcons: true,
+						precomposedIcons: false,
+						declareOnlyDefaultIcon: true,
+					},
+				},
+				desktopBrowser: {},
+				windows: {
+					pictureAspect: 'noChange',
+					backgroundColor: config.THEME_BACKGROUND,
+					onConflict: 'override',
+					assets: {
+						windows80Ie10Tile: false,
+						windows10Ie11EdgeTiles: {
+							small: false,
+							medium: true,
+							big: false,
+							rectangle: false,
+						},
+					},
+				},
+				androidChrome: {
+					pictureAspect: 'noChange',
+					themeColor: config.THEME_COLOR,
+					manifest: {
+						name: config.COMPANY_NAME,
+						short_name: config.COMPANY_NAME_SHORT,
+						description: config.COMPANY_DESCRIPTION,
+						start_url: '/index.html',
+						background_color: config.THEME_BACKGROUND,
+						display: 'standalone',
+						orientation: 'notSet',
+						onConflict: 'override',
+						declared: true,
+					},
+					assets: {
+						legacyIcon: false,
+						lowResolutionIcons: true,
+					},
+				},
+				safariPinnedTab: {
+					pictureAspect: 'blackAndWhite',
+					threshold: 60,
+					themeColor: config.THEME_COLOR,
+				},
+			},
+			settings: {
+				compression: 3,
+				scalingAlgorithm: 'Mitchell',
+				errorOnImageTooSmall: false,
+			},
+		}, done)
+	}
+	done()
+}
