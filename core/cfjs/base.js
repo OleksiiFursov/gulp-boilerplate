6c75f8952eb83da51ccf84193d52b8a71c50cc16
OleksiiFursov <nodepro777@gmail.com>
2025-10-17 10:49:57 +0200

    Upgrade

--- core/cfjs/base.js
+++ core/cfjs/base.js
@@ -1,116 +1,109 @@
-import {animAfter} from './animate.js'
+import { animAfter } from './animate.js'
 
-
-export let theLoaded;
+export let theLoaded
 
 window.d = document
 window.body = d.body
 window.bc = body.classList
 
-
 window.cfjsConfig = {}
-export const detectDevice = () => innerWidth < window.cfjsConfig.mobileMaxWidth || innerWidth < innerHeight;
-window.isMob = detectDevice();
-
+export const detectDevice = () => innerWidth < window.cfjsConfig.mobileMaxWidth || innerWidth < innerHeight
+window.isMob = detectDevice()
 
 // Animate:
 export const init = ({
-                         delayLoading = 100,
-                         mobileMaxWidth = 980,
-                         onScrollThrottle = 10,
-                         onMouseMoveThrottle = 10,
-                         onSwipeMoveThrottle = 10,
-                         loadedAnimAfter,
-                         preloader = false,
-                         preloaderText = 'Loading...'
-                     } = {}) => {
+	delayLoading = 100,
+	mobileMaxWidth = 980,
+	onScrollThrottle = 10,
+	onMouseMoveThrottle = 10,
+	onSwipeMoveThrottle = 10,
+	loadedAnimAfter,
+	preloader = false,
+	preloaderText = 'Loading...',
+} = {}) => {
 
-    $o('.page-top', e => {
-        bc.toggle('is-scroll', e.intersectionRatio === 0);
-    })
-    // Preloader
+	$o('.page-top', e => {
+		bc.toggle('is-scroll', e.intersectionRatio === 0)
+	})
+	// Preloader
 
-    let preloaderTimer;
-    if (preloader) {
-        preloaderTimer = setTimeout(() => {
-            $('.preloader').textContent = preloaderText
-        }, 300);
-    }
+	let preloaderTimer
+	if (preloader) {
+		preloaderTimer = setTimeout(() => {
+			$('.preloader').textContent = preloaderText
+		}, 300)
+	}
 
+	Object.assign(cfjsConfig, {
+		mobileMaxWidth,
+		onScrollThrottle,
+		onMouseMoveThrottle,
+		onSwipeMoveThrottle,
+	})
 
-    Object.assign(cfjsConfig, {
-        mobileMaxWidth,
-        onScrollThrottle,
-        onMouseMoveThrottle,
-        onSwipeMoveThrottle,
-    });
+	$e(window, 'load', () => {
+		setTimeout(() => {
+			bc.add('loaded')
+			if (preloader)
+				clearTimeout(preloaderTimer)
+			if (loadedAnimAfter) {
+				animAfter(loadedAnimAfter, theLoaded)
+			} else {
+				theLoaded()
+			}
 
-    $e(window, 'load', () => {
-        setTimeout(() => {
-            bc.add('loaded');
-            if (preloader)
-                clearTimeout(preloaderTimer);
-            if (loadedAnimAfter) {
-                animAfter(loadedAnimAfter, theLoaded)
-            } else {
-                theLoaded()
-            }
-
-        }, delayLoading)
-    })
+		}, delayLoading)
+	})
 
 }
 export const isLoaded = new Promise(e => theLoaded = e)
-export const $ = (sel, p = d) => p.querySelector(sel);
+export const $ = (sel, p = d) => p.querySelector(sel)
 export const $$ = (sel, p = d) => p.querySelectorAll(sel)
-export const func = () => 1;
+export const func = () => 1
 export const getElement = (sel, p = d) => (typeof sel === 'string' ? $(sel, p) : sel)
 
+export const interacted = {}
+const interactionPromise = {}
 
-
-
-export const interacted = {};
-const interactionPromise={};
-
-export function onUserInteraction(call, events=["click", "keydown", "mousemove", "touchstart", "scroll"]) {
+export function onUserInteraction (call, events = ['click', 'keydown', 'mousemove', 'touchstart', 'scroll']) {
 	return (() => {
-		const keys = events.sort().join();
-		if (interacted[keys]) return Promise.resolve();
+		const keys = events.sort().join()
+		if (interacted[keys]) return Promise.resolve()
 		if (!interactionPromise[keys]) {
 			interactionPromise[keys] = new Promise((resolve) => {
 				const handler = () => {
-					interacted[keys] = true;
+					interacted[keys] = true
 					$each(events, (ev) =>
-					  $eR(window, ev, handler)
-					);
-					resolve();
-				};
+					  $eR(window, ev, handler),
+					)
+					resolve()
+				}
 
 				events.forEach((ev) =>
-				  $e(window, ev, handler, { once: true })
-				);
-			});
+				  $e(window, ev, handler, { once: true }),
+				)
+			})
 		}
 
-		return interactionPromise[keys];
-	})().then(call);
+		return interactionPromise[keys]
+	})().then(call)
 }
 
-export const $each = (sel, call, p = d) => (Array.isArray(sel) ? sel: $$(sel, p)).forEach(call)
-export const $o = (sel, func, params = {rootMargin: '0px', threshold: 0.2}) => {
-    const el = getElement(sel)
-    if (el) {
-        const r = new IntersectionObserver(([e]) => {
-            func(e, e.target)
-        }, params)
-        r.observe(el)
-        return r
-    }
+export const $each = (sel, call, p = d) => (Array.isArray(sel) || sel instanceof NodeList ? sel : $$(sel, p)).forEach(call)
+export const $o = (sel, func, params = { rootMargin: '0px', threshold: 0.2 }) => {
+	const el = getElement(sel)
+	if (el) {
+		const r = new IntersectionObserver(([e]) => {
+			func(e, e.target)
+		}, params)
+		r.observe(el)
+		return r
+	}
 }
 
 export const $e = (sel, type, call, options) => {
-    const el = typeof sel === 'string' ? $(sel) : sel
-    el && el.addEventListener(type, call, options)
+	const el = typeof sel === 'string' ? $(sel) : sel
+	el && el.addEventListener(type, call, options)
 }
 
 export const $eR = (sel, type, call) => {
@@ -118,28 +111,40 @@ export const $eR = (sel, type, call) => {
 	el && el.removeEventListener(type, call)
 }
 export const $v = (sel, call, once = false) => {
-    isLoaded.then(() => {
-        let obs = $o(sel, e => {
-            if (e.isIntersecting) {
-                call(sel)
-                if (once) {
-                    obs.unobserve(getElement(sel))
-                }
-            }
+	isLoaded.then(() => {
+		let obs = $o(sel, e => {
+			if (e.isIntersecting) {
+				call(sel)
+				if (once) {
+					obs.unobserve(getElement(sel))
+				}
+			}
 
-        })
-    })
+		})
+	})
 }
 
-export const css = (sel, prop, value=undefined, unit = 'rem') => {
-    const $el = getElement(sel);
-    const st =  getComputedStyle($el);
-    if(value === undefined) return st[prop];
+export const css = (sel, prop, value = undefined, unit = 'rem') => {
+	const $el = getElement(sel)
 
-    if (!isNaN(value)) {
-        value += unit;
-    }
-    $el.style[prop] = value;
+	if (value === undefined){
+		const st = getComputedStyle($el)
+		return st[prop]
+	}
+
+	const exlProp = ['line-height', 'lineHeight', 'opacity', 'flex-grow', 'scale', 'zoom', 'flexGrow', 'flex', 'z-index', 'zIndex', 'order', 'orphans', 'widows', 'tab-size', 'tabSize', 'animation-iteration-count', 'animationIterationCount'];
+	if (typeof value === 'number' && value !== 0 && !exlProp.includes(value))
+	{
+		value += unit
+	}
+	$el.style[prop] = value
+}
+
+export const setCSS = (selector, props) => {
+	const $el = getElement(selector)
+	for (const prop in props) {
+		css($el, prop, props[prop]);
+	}
 }
 
 // export const icss = (sel, prop, value, unit='rem') => {
